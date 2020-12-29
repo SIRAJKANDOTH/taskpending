d93e9c3fe3969c105c7944a468755820f3764504
Arun Sunny <as@ddc.dev>
2020-12-29 10:46:25 +0530

    fixed token address not showing issue

--- migrations/2_deploy_yrt.js
+++ migrations/2_deploy_yrt.js
@@ -1,7 +1,14 @@
 var yrToken = artifacts.require("./yrToken.sol");
 var GnosisSafe = artifacts.require("./GnosisSafe.sol");
 
-module.exports =  async function(deployer) {
-   let token = await deployer.deploy(yrToken,400000000000);
-   await deployer.deploy(GnosisSafe,token);
+// module.exports =  async function(deployer) {
+//    let token = await deployer.deploy(yrToken,400000000000);
+//    await deployer.deploy(GnosisSafe,token);
+// };
+
+module.exports =  function(deployer) {
+   deployer.deploy(yrToken,400000000000)
+   .then(()=>{
+      return deployer.deploy(GnosisSafe,yrToken.address)
+   });
 };
