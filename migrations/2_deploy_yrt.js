cb5588a55c1dc7177ea20b011d10b8e00787ab2b
rashiq-ddc <mr@ddc.dev>
2021-01-06 17:04:47 +0530

    ap contract deployment

--- migrations/2_deploy_yrt.js
+++ migrations/2_deploy_yrt.js
@@ -1,18 +1,13 @@
 var yrToken = artifacts.require("./yrToken.sol");
 var GnosisSafe = artifacts.require("./GnosisSafe.sol");
 var Whitelist = artifacts.require("./whitelist/Whitelist.sol");
+var APContract = artifacts.require("./aps/APContract.sol");
 
-// @NOTE: Deploy the token for test purpose (not required in production)
-// module.exports = (deployer) => {
-//   var wL = deployer.deploy(Whitelist);
-//   var tkn = deployer.deploy(yrToken, 10000000000000);
-//   Promise.all([wL, tkn]).then((res) => {
-//     console.log("--------------------------------------------------------Response",res)
-//     // await deployer.deploy(GnosisSafe, res[0].address, res[1].address);
-//   });
-// };
+
 module.exports = async(deployer) => {
   deployer.deploy(Whitelist).then(async(whLst)=>{
+    await deployer.deploy(APContract);
+    
     var tkn = await deployer.deploy(yrToken, 10000000000000);
     await deployer.deploy(GnosisSafe, tkn.address,whLst.address);
   });
@@ -20,11 +15,4 @@ module.exports = async(deployer) => {
 
 };
 
-// module.exports = async (deployer) => {
-//   deployer.deploy(Whitelist)
-//   deployer.deploy(yrToken, 10000000000000).then(async (token) => {
-//     // await deployer.deploy(GnosisSafe, token.address);
-//     console.log(token)
-//   });
-// };
 
