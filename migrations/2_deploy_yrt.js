33f263bc14405fc326d18c876f433fa0c9eb3868
Nandu Raj <nr@ddc.dev>
2021-01-28 12:32:17 +0530

    edited the migration script to accomodate changes

--- migrations/2_deploy_yrt.js
+++ migrations/2_deploy_yrt.js
@@ -1,15 +1,28 @@
 var GnosisSafe = artifacts.require("./GnosisSafe.sol");
 var Whitelist = artifacts.require("./whitelist/Whitelist.sol");
 var APContract = artifacts.require("./aps/APContract.sol");
+var ProxyFactory = artifacts.require("./proxies/GnosisSafeProxyFactory.sol");
 
 module.exports = async (deployer) => {
 	await deployer.deploy(GnosisSafe);
-	const gnosisSafe = await GnosisSafe.deployed();
+	const gnosisSafeMasterCopy = await GnosisSafe.deployed();
 
 	await deployer.deploy(Whitelist);
 	const whitelist = await Whitelist.deployed();
 
-	await deployer.deploy(APContract,gnosisSafe.address, whitelist.address);
+	await deployer.deploy(
+		APContract,
+		gnosisSafeMasterCopy.address,
+		whitelist.address
+	);
 	const apContract = await APContract.deployed();
-	
+
+	await deployer.deploy(
+		ProxyFactory,
+		gnosisSafeMasterCopy.address,
+		apContract.address
+	);
+	const proxyFactory = await ProxyFactory.deployed();
+
+	await apContract.addProxyFactory(proxyFactory.address);
 };
