f15d00c0f13c7fe060354fdb857ee5ae264f8f6c
Nandu Raj <nr@ddc.dev>
2021-02-05 15:58:15 +0530

    updated migration script

--- migrations/2_deploy_yrt.js
+++ migrations/2_deploy_yrt.js
@@ -25,19 +25,22 @@ module.exports = async (deployer) => {
 	const proxyFactory = await ProxyFactory.deployed();
 
 	await apContract.addProxyFactory(proxyFactory.address);
-	await apContract.addAsset("TUSD","Tether USD","0x6f7454cba97fffe10e053187f23925a86f5c20c4","0xdac17f958d2ee523a2206206994597c13d831ec7")
-	await apContract.addAsset("DAI","DAI Coin","0x2bA49Aaa16E6afD2a993473cfB70Fa8559B523cF","0x6b175474e89094c44da98b954eedeac495271d0f")
-	await apContract.addAsset("USDC","USD Coin","0xa24de01df22b63d23Ebc1882a5E3d4ec0d907bFB","0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48")
+	await apContract.addAsset("TUSD","Tether USD","0x6f7454cba97fffe10e053187f23925a86f5c20c4","0x7bee4c1408c6461ee35a9027ea6007e7d1764036")
+	await apContract.addAsset("DAI","DAI Coin","0x2bA49Aaa16E6afD2a993473cfB70Fa8559B523cF","0x95b58a6bff3d14b7db2f5cb5f0ad413dc2940658")
+	await apContract.addAsset("USDC","USD Coin","0xa24de01df22b63d23Ebc1882a5E3d4ec0d907bFB","0x7d66cde53cc0a169cae32712fc48934e610aef14")
 	await apContract.addAsset("LINK","LINK Coin","0xd8bD0a1cB028a31AA859A21A3758685a95dE4623","0x01be23585060835e02b77ef475b0cc51aa1e0709")
+	await apContract.addAsset("BAT","Basic Attention Token","0x031dB56e01f82f20803059331DC6bEe9b17F7fC9","0x2fa6a0728a63115e6fc1eb8496ea94e86b8cdf7b")
+	await apContract.addAsset("BNB","Binance Coin","0xcf0f51ca2cDAecb464eeE4227f5295F2384F84ED","0x030b0a08ecadde5ac33859a48d87416946c966a1")
+	await apContract.addAsset("fnx","FinanceX token","0xcf74110A02b1D391B27cE37364ABc3b279B1d9D1","0xd729a77e319e059b4467c402e173c552e63a6c55")
 
-	await apContract.addProtocol("yearn Curve.fi GUSD/3Crv","crvGUSD","0xcC7E70A958917cCe67B4B87a8C30E6297451aE98")
-	await apContract.addProtocol("yearn Curve.fi MUSD/3Crv","crvMUSD","0x0FCDAeDFb8A7DfDa2e9838564c5A1665d856AFDF")
-	await apContract.addProtocol("yearn Curve.fi cDAI/cUSDC","crvCOMP","0x629c759D1E83eFbF63d84eb3868B564d9521C129")
-	await apContract.addProtocol("yearn yearn.finance","YFI","0xBA2E7Fed597fd0E3e70f5130BcDbbFE06bB94fe1")
-	await apContract.addProtocol("HEGIC yVault","HEGIC","0xe11ba472F74869176652C35D30dB89854b5ae84D")
+	await apContract.addProtocol("yearn Curve.fi GUSD/3Crv","crvGUSD","0x95b58a6bff3d14b7db2f5cb5f0ad413dc2940658")
+	await apContract.addProtocol("yearn Curve.fi MUSD/3Crv","crvMUSD","0x7d66cde53cc0a169cae32712fc48934e610aef14")
+	await apContract.addProtocol("yearn Curve.fi cDAI/cUSDC","crvCOMP","0xfb1d709cb959ac0ea14cad0927eabc7832e65058")
+	await apContract.addProtocol("yearn yearn.finance","YFI","0x01be23585060835e02b77ef475b0cc51aa1e0709")
+	await apContract.addProtocol("HEGIC yVault","HEGIC","0x2fa6a0728a63115e6fc1eb8496ea94e86b8cdf7b")
 
-	await apContract.addStrategy("Yearn it All", "0x6f7454cba97fffe10e053187f23925a86f5c20c4",[])
-	await apContract.addStrategy("Smart Deposit", "0x2bA49Aaa16E6afD2a993473cfB70Fa8559B523cF",[])
-	await apContract.addStrategy("Smart Withdraw", "0xa24de01df22b63d23Ebc1882a5E3d4ec0d907bFB",[])
+	await apContract.addStrategy("Yearn it All", "0x6f7454cba97fffe10e053187f23925a86f5c20c4",["0x95b58a6bff3d14b7db2f5cb5f0ad413dc2940658", "0x7d66cde53cc0a169cae32712fc48934e610aef14"])
+	await apContract.addStrategy("Smart Deposit", "0x2bA49Aaa16E6afD2a993473cfB70Fa8559B523cF",["0x95b58a6bff3d14b7db2f5cb5f0ad413dc2940658","0x7d66cde53cc0a169cae32712fc48934e610aef14"])
+	await apContract.addStrategy("Smart Withdraw", "0xa24de01df22b63d23Ebc1882a5E3d4ec0d907bFB",["0x95b58a6bff3d14b7db2f5cb5f0ad413dc2940658","0x7d66cde53cc0a169cae32712fc48934e610aef14"])
 
 };
