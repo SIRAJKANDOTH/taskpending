18a02ac6b3b1caa85057dd1ea8d4180d25d45b9f
rashiq-ddc <mr@ddc.dev>
2021-02-05 10:35:35 +0530

    yearn-it-all

--- contracts/strategies/DummyStrategy.sol
+++ contracts/strategies/DummyStrategy.sol
@@ -3,8 +3,8 @@ import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 contract DummyStrategy{
     using SafeERC20 for IERC20;
 
-    function deposit(uint256 _amount) external {
-        // IERC20(_token).transferFrom();
+    function deposit() external {
+
     }
     
 
--- contracts/strategies/YearnItAll.sol
+++ contracts/strategies/YearnItAll.sol
@@ -5,12 +5,12 @@ import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
 import "../interfaces/yearn/IVault.sol";
 
-contract LivaOne{
+contract YearnItAll{
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
     // yearn vault - need to confirm address
     address public constant want = address(0x845838DF265Dcd2c412A1Dc9e959c7d08537f8a2);
-   address private crvComp=address(0x2994529C0652D127b7842094103715ec5299bBed);
+    address private crvComp=address(0x2994529C0652D127b7842094103715ec5299bBed);
 
    function deposit(uint256 _amount) external {
         IVault(crvComp).deposit(_amount);
