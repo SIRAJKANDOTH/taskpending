6e9eba3c8dcf5054585278ae24c830c1af019974
Nandu Raj <nr@ddc.dev>
2021-02-08 19:22:00 +0530

    removed usdt from aps

--- migrations/2_deploy_yrt.js
+++ migrations/2_deploy_yrt.js
@@ -25,7 +25,6 @@ module.exports = async (deployer) => {
 	const proxyFactory = await ProxyFactory.deployed();
 
 	await apContract.addProxyFactory(proxyFactory.address);
-	await apContract.addAsset("TUSD","Tether USD","0x6f7454cba97fffe10e053187f23925a86f5c20c4","0xd9ba894e0097f8cc2bbc9d24d308b98e36dc6d02")
 	await apContract.addAsset("DAI","DAI Coin","0x2bA49Aaa16E6afD2a993473cfB70Fa8559B523cF","0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea")
 	await apContract.addAsset("USDC","USD Coin","0xa24de01df22b63d23Ebc1882a5E3d4ec0d907bFB","0x4dbcdf9b62e891a7cec5a2568c3f4faf9e8abe2b")
 	await apContract.addAsset("LINK","LINK Coin","0xd8bD0a1cB028a31AA859A21A3758685a95dE4623","0x01be23585060835e02b77ef475b0cc51aa1e0709")
