329258c6b3fcc0416ea5faeef815ac7c0bae4625
arun-ddc <as@ddc.dev>
2021-02-10 19:48:20 +0530

    Added newer string utils

--- contracts/utils/InstructionOracle.sol
+++ contracts/utils/InstructionOracle.sol
@@ -1,21 +1,24 @@
 pragma solidity >=0.5.0 <0.7.0;
-
+pragma experimental ABIEncoderV2;
 import "./provable/Provable.sol";
-
+import "./StringUtils.sol";
 contract InstructionOracle is usingProvable {
+    using Strings for string;
     string public response;
     string public request;
     string dataSource = "IPFS";
+    string[] public strInstructions;
     event LogConstructorInitiated(string nextStep);
     event LogDataUpdated(string price);
     event LogNewProvableQuery(string description);
 
     // address public owner;
-    uint gasLimit = 200000;
+    uint256 gasLimit = 200000;
 
     function __callback(bytes32 myid, string memory result) public {
         if (msg.sender != provable_cbAddress()) revert();
         response = result;
+        strInstructions = split(result);
         emit LogDataUpdated(result);
     }
 
@@ -23,10 +26,15 @@ contract InstructionOracle is usingProvable {
         request = _str;
     }
 
-    function setCustomGasLimit(uint _gas) public {
+    function setCustomGasLimit(uint256 _gas) public {
         // TO CONFIRM:- WHO CAN SET GAS LIMIT ?
         // require(msg.sender == owner, "Unauthorized");
         gasLimit = _gas;
+    }
+
+    function split(string memory _str) public pure returns (string[] memory) {
+        string[] memory str = _str.split(",");
+        return str;
     }
 
     function update() public payable {
