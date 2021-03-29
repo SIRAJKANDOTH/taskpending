c8fb62516fc842abb63b6d28f25257fb45c2feb3
Nandu Raj <nr@ddc.dev>
2021-03-29 20:00:20 +0530

    modified deployment script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,169 +3,192 @@ const Whitelist = artifacts.require("./whitelist/Whitelist.sol");
 const APContract = artifacts.require("./aps/APContract.sol");
 const PriceModule = artifacts.require("./price/PriceModule.sol");
 const ProxyFactory = artifacts.require(
-  "./proxies/YieldsterVaultProxyFactory.sol"
+	"./proxies/YieldsterVaultProxyFactory.sol"
 );
 const PlatformManagementFee = artifacts.require(
-  "./delegateContracts/ManagementFee.sol"
+	"./delegateContracts/ManagementFee.sol"
 );
 const YearnItAll = artifacts.require("./strategies/YearnItAll.sol");
+const LivaOne = artifacts.require("./strategies/LivaOne.sol");
 const YearnItAllMinter = artifacts.require("./strategies/YearnItAllMinter.sol");
+const LivaOneMinter = artifacts.require("./strategies/LivaOneMinter.sol");
 const HexUtils = artifacts.require("./utils/HexUtils.sol");
 const StockDeposit = artifacts.require(
-  "./smartStrategies/deposit/StockDeposit.sol"
+	"./smartStrategies/deposit/StockDeposit.sol"
 );
 const StockWithdraw = artifacts.require(
-  "./smartStrategies/deposit/StockWithdraw.sol"
+	"./smartStrategies/deposit/StockWithdraw.sol"
 );
 const Exchange = artifacts.require("./exchange/Exchange.sol");
 
 module.exports = async (deployer) => {
-  await deployer.deploy(YieldsterVault);
-  const yieldsterVaultMasterCopy = await YieldsterVault.deployed();
+	await deployer.deploy(YieldsterVault);
+	const yieldsterVaultMasterCopy = await YieldsterVault.deployed();
 
-  await deployer.deploy(PlatformManagementFee);
-  const managementFee = await PlatformManagementFee.deployed();
+	await deployer.deploy(PlatformManagementFee);
+	const managementFee = await PlatformManagementFee.deployed();
 
-  await deployer.deploy(Whitelist);
-  const whitelist = await Whitelist.deployed();
+	await deployer.deploy(Whitelist);
+	const whitelist = await Whitelist.deployed();
 
-  await deployer.deploy(HexUtils);
-  const hexUtils = await HexUtils.deployed();
+	await deployer.deploy(HexUtils);
+	const hexUtils = await HexUtils.deployed();
 
-  await deployer.deploy(
-    APContract,
-    yieldsterVaultMasterCopy.address,
-    whitelist.address,
-    managementFee.address,
-    hexUtils.address
-  );
-  const apContract = await APContract.deployed();
+	await deployer.deploy(
+		APContract,
+		yieldsterVaultMasterCopy.address,
+		whitelist.address,
+		managementFee.address,
+		hexUtils.address
+	);
+	const apContract = await APContract.deployed();
 
-  await deployer.deploy(StockDeposit);
-  const stockDeposit = await StockDeposit.deployed();
+	await deployer.deploy(StockDeposit);
+	const stockDeposit = await StockDeposit.deployed();
 
-  await deployer.deploy(StockWithdraw);
-  const stockWithdraw = await StockWithdraw.deployed();
+	await deployer.deploy(StockWithdraw);
+	const stockWithdraw = await StockWithdraw.deployed();
 
-  await deployer.deploy(Exchange);
-  const exchange = await Exchange.deployed();
+	await deployer.deploy(Exchange);
+	const exchange = await Exchange.deployed();
 
-  await deployer.deploy(PriceModule, apContract.address);
-  const priceModule = await PriceModule.deployed();
+	await deployer.deploy(PriceModule, apContract.address);
+	const priceModule = await PriceModule.deployed();
 
-  await apContract.setPriceModule(priceModule.address);
+	await apContract.setPriceModule(priceModule.address);
 
-  await deployer.deploy(
-    ProxyFactory,
-    yieldsterVaultMasterCopy.address,
-    apContract.address
-  );
-  const proxyFactory = await ProxyFactory.deployed();
+	//Deploying Proxy Factory
+	await deployer.deploy(
+		ProxyFactory,
+		yieldsterVaultMasterCopy.address,
+		apContract.address
+	);
+	const proxyFactory = await ProxyFactory.deployed();
 
+	//Deploying Yearn It All strategy
+	await deployer.deploy(YearnItAll, apContract.address, [
+		"0xd27Dc2D8ceF541f94FbA737079F2DFeA39B2EEf8",
+		"0xD8052918CAd9a8B3a564d7Aa4e680a0dc156380e",
+		"0x3662ABD754eE1d8CB6f5F1D4E315932b36e9955B",
+	]);
+	const yearnItAll = await YearnItAll.deployed();
+	await deployer.deploy(
+		YearnItAllMinter,
+		apContract.address,
+		yearnItAll.address
+	);
+	const yearnItAllMinter = await YearnItAllMinter.deployed();
 
-  await deployer.deploy(YearnItAll, apContract.address, [
-    "0xd27Dc2D8ceF541f94FbA737079F2DFeA39B2EEf8",
-    "0xD8052918CAd9a8B3a564d7Aa4e680a0dc156380e",
-    "0x3662ABD754eE1d8CB6f5F1D4E315932b36e9955B",
-  ]);
+	//Deploying Liva One strategy
+	await deployer.deploy(LivaOne, apContract.address, [
+		"0xd27Dc2D8ceF541f94FbA737079F2DFeA39B2EEf8",
+		"0xD8052918CAd9a8B3a564d7Aa4e680a0dc156380e",
+		"0x3662ABD754eE1d8CB6f5F1D4E315932b36e9955B",
+	]);
+	const livaOne = await LivaOne.deployed();
+	await deployer.deploy(LivaOneMinter, apContract.address, livaOne.address);
+	const livaOneMinter = await LivaOneMinter.deployed();
 
-  const yearnItAll = await YearnItAll.deployed();
+	await apContract.setYieldsterExchange(exchange.address);
 
-  await deployer.deploy(
-    YearnItAllMinter,
-    apContract.address,
-    yearnItAll.address
-  );
-  const yearnItAllMinter =await YearnItAllMinter.deployed();
-  await apContract.setYieldsterExchange(exchange.address);
+	await apContract.setStockDepositWithdraw(
+		stockDeposit.address,
+		stockWithdraw.address
+	);
 
-  await apContract.setStockDepositWithdraw(
-    stockDeposit.address,
-    stockWithdraw.address
-  );
-//   await apContract.setStrategyMinter(yearnItAllMinter.address);
+	await apContract.addProxyFactory(proxyFactory.address);
 
-//   await apContract.setStrategyExecutor(
-//     "0x92506Ee00ad88354fa25E6CbFa7d42116d6823C0"
-//   );
+	//adding assets
+	await apContract.addAsset(
+		"DAI",
+		"DAI Coin",
+		"0x2bA49Aaa16E6afD2a993473cfB70Fa8559B523cF",
+		"0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea"
+	);
+	await apContract.addAsset(
+		"USDC",
+		"USD Coin",
+		"0xa24de01df22b63d23Ebc1882a5E3d4ec0d907bFB",
+		"0x4dbcdf9b62e891a7cec5a2568c3f4faf9e8abe2b"
+	);
+	await apContract.addAsset(
+		"LINK",
+		"LINK Coin",
+		"0xd8bD0a1cB028a31AA859A21A3758685a95dE4623",
+		"0x01be23585060835e02b77ef475b0cc51aa1e0709"
+	);
+	await apContract.addAsset(
+		"BNB",
+		"Binance Coin",
+		"0xcf0f51ca2cDAecb464eeE4227f5295F2384F84ED",
+		"0x030b0a08ecadde5ac33859a48d87416946c966a1"
+	);
+	await apContract.addAsset(
+		"fnx",
+		"FinanceX token",
+		"0xcf74110A02b1D391B27cE37364ABc3b279B1d9D1",
+		"0xd729a77e319e059b4467c402e173c552e63a6c55"
+	);
 
-  await apContract.addProxyFactory(proxyFactory.address);
+	//adding protocols in the assets for feed address
 
-  //adding assets
-  await apContract.addAsset(
-    "DAI",
-    "DAI Coin",
-    "0x2bA49Aaa16E6afD2a993473cfB70Fa8559B523cF",
-    "0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea"
-  );
-  await apContract.addAsset(
-    "USDC",
-    "USD Coin",
-    "0xa24de01df22b63d23Ebc1882a5E3d4ec0d907bFB",
-    "0x4dbcdf9b62e891a7cec5a2568c3f4faf9e8abe2b"
-  );
-  await apContract.addAsset(
-    "LINK",
-    "LINK Coin",
-    "0xd8bD0a1cB028a31AA859A21A3758685a95dE4623",
-    "0x01be23585060835e02b77ef475b0cc51aa1e0709"
-  );
-  await apContract.addAsset(
-    "BNB",
-    "Binance Coin",
-    "0xcf0f51ca2cDAecb464eeE4227f5295F2384F84ED",
-    "0x030b0a08ecadde5ac33859a48d87416946c966a1"
-  );
-  await apContract.addAsset(
-    "fnx",
-    "FinanceX token",
-    "0xcf74110A02b1D391B27cE37364ABc3b279B1d9D1",
-    "0xd729a77e319e059b4467c402e173c552e63a6c55"
-  );
+	await apContract.addAsset(
+		"yearn Curve.fi crvCOMP",
+		"crvCOMP",
+		"0x2bA49Aaa16E6afD2a993473cfB70Fa8559B523cF",
+		"0xd27Dc2D8ceF541f94FbA737079F2DFeA39B2EEf8"
+	);
+	await apContract.addAsset(
+		"yearn Curve.fi GUSD/3Crv",
+		"crvGUSD",
+		"0xd8bD0a1cB028a31AA859A21A3758685a95dE4623",
+		"0xD8052918CAd9a8B3a564d7Aa4e680a0dc156380e"
+	);
+	await apContract.addAsset(
+		"yearn Curve.fi MUSD/3Crv",
+		"crvMUSD",
+		"0xa24de01df22b63d23Ebc1882a5E3d4ec0d907bFB",
+		"0x3662ABD754eE1d8CB6f5F1D4E315932b36e9955B"
+	);
 
-  //adding protocols in the assets for feed address
+	//adding protocols
+	await apContract.addProtocol(
+		"yearn Curve.fi crvCOMP",
+		"crvCOMP",
+		"0xd27Dc2D8ceF541f94FbA737079F2DFeA39B2EEf8"
+	);
+	await apContract.addProtocol(
+		"yearn Curve.fi GUSD/3Crv",
+		"crvGUSD",
+		"0xD8052918CAd9a8B3a564d7Aa4e680a0dc156380e"
+	);
+	await apContract.addProtocol(
+		"yearn Curve.fi MUSD/3Crv",
+		"crvMUSD",
+		"0x3662ABD754eE1d8CB6f5F1D4E315932b36e9955B"
+	);
 
-  await apContract.addAsset(
-    "yearn Curve.fi crvCOMP",
-    "crvCOMP",
-    "0x2bA49Aaa16E6afD2a993473cfB70Fa8559B523cF",
-    "0xd27Dc2D8ceF541f94FbA737079F2DFeA39B2EEf8"
-  );
-  await apContract.addAsset(
-    "yearn Curve.fi GUSD/3Crv",
-    "crvGUSD",
-    "0xd8bD0a1cB028a31AA859A21A3758685a95dE4623",
-    "0xD8052918CAd9a8B3a564d7Aa4e680a0dc156380e"
-  );
-  await apContract.addAsset(
-    "yearn Curve.fi MUSD/3Crv",
-    "crvMUSD",
-    "0xa24de01df22b63d23Ebc1882a5E3d4ec0d907bFB",
-    "0x3662ABD754eE1d8CB6f5F1D4E315932b36e9955B"
-  );
-
-  //adding protocols
-  await apContract.addProtocol(
-    "yearn Curve.fi crvCOMP",
-    "crvCOMP",
-    "0xd27Dc2D8ceF541f94FbA737079F2DFeA39B2EEf8"
-  );
-  await apContract.addProtocol(
-    "yearn Curve.fi GUSD/3Crv",
-    "crvGUSD",
-    "0xD8052918CAd9a8B3a564d7Aa4e680a0dc156380e"
-  );
-  await apContract.addProtocol(
-    "yearn Curve.fi MUSD/3Crv",
-    "crvMUSD",
-    "0x3662ABD754eE1d8CB6f5F1D4E315932b36e9955B"
-  );
-//  add accounts
-  await apContract.addStrategy("Yearn it All", yearnItAll.address, [
-    "0xd27Dc2D8ceF541f94FbA737079F2DFeA39B2EEf8",
-    "0xD8052918CAd9a8B3a564d7Aa4e680a0dc156380e",
-    "0x3662ABD754eE1d8CB6f5F1D4E315932b36e9955B",
-  ],
-  yearnItAllMinter.address,
-  "0xaC86730Bd6d5d68e004f59C0BC940b92b99D3aEe");
+	//adding strategy to AP contract
+	await apContract.addStrategy(
+		"Yearn it All",
+		yearnItAll.address,
+		[
+			"0xd27Dc2D8ceF541f94FbA737079F2DFeA39B2EEf8",
+			"0xD8052918CAd9a8B3a564d7Aa4e680a0dc156380e",
+			"0x3662ABD754eE1d8CB6f5F1D4E315932b36e9955B",
+		],
+		yearnItAllMinter.address,
+		"0x92506Ee00ad88354fa25E6CbFa7d42116d6823C0"
+	);
+	await apContract.addStrategy(
+		"Liva One",
+		livaOne.address,
+		[
+			"0xd27Dc2D8ceF541f94FbA737079F2DFeA39B2EEf8",
+			"0xD8052918CAd9a8B3a564d7Aa4e680a0dc156380e",
+			"0x3662ABD754eE1d8CB6f5F1D4E315932b36e9955B",
+		],
+		livaOneMinter.address,
+		"0x92506Ee00ad88354fa25E6CbFa7d42116d6823C0"
+	);
 };
