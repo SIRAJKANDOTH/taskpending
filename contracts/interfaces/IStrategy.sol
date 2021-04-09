f0c6161894f174618eb67f5d0ce2002842be9bd5
rashiq-ddc <mr@ddc.dev>
2021-04-09 19:06:53 +0530

    zapper yearnit all

--- contracts/interfaces/IStrategy.sol
+++ contracts/interfaces/IStrategy.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.5.0 <0.7.0;
 interface IStrategy {
     function want() external view returns (address);
 
+    function deposit(address[] calldata,uint256[] calldata) external;
     function deposit(uint256) external;
 
     function withdraw(uint256) external;
