64ca1b3d339320256bd2c4e7c91f6507a14568d3
Rashiq <mr@ddc.dev>
2021-04-20 13:36:28 +0530

    added fromDecimals and toDecimals function for token count conversion

--- contracts/utils/HexUtils.sol
+++ contracts/utils/HexUtils.sol
@@ -1,18 +1,29 @@
 pragma solidity >=0.5.0 <0.7.0;
-contract HexUtils{
-    function fromHex(bytes memory ss) public pure returns (bytes memory) 
+import "../token/ERC20Detailed.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+contract HexUtils
+{
+    using SafeMath for uint256;
+
+    function fromHex(bytes memory ss) 
+        public 
+        pure 
+        returns(bytes memory) 
     {
         require(ss.length%2 == 0); // length must be even
         bytes memory r = new bytes(ss.length/2);
-        for (uint i=0; i<ss.length/2; ++i) 
-        {
-            r[i] = bytes1(fromHexChar(uint8(ss[2*i])) * 16 +
-                fromHexChar(uint8(ss[2*i+1])));
+        for (uint i = 0; i < ss.length/2; ++i) {
+            r[i] = bytes1(fromHexChar(uint8(ss[2*i])) * 16 + fromHexChar(uint8(ss[2*i+1])));
             }
         return r;
     }
     
-    function fromHexChar(uint8 c) public pure returns (uint8) {
+    function fromHexChar(uint8 c) 
+        public 
+        pure 
+        returns(uint8) 
+    {
         if (bytes1(c) >= bytes1('0') && bytes1(c) <= bytes1('9')) {
             return c - uint8(bytes1('0'));
         }
@@ -23,4 +34,36 @@ contract HexUtils{
             return 10 + c - uint8(bytes1('A'));
         }
     }
+
+    function toDecimals(address tokenAddress, uint256 tokenCount)
+        public
+        view
+        returns (uint256)
+    {
+        uint8 decimals = ERC20Detailed(tokenAddress).decimals();
+        if(decimals < 18) {
+            return tokenCount.mul(uint256(18 - decimals));
+        } else if (decimals > 18) {
+            return tokenCount.div(uint256(decimals - 18));
+        } else {
+            return tokenCount;
+        }
+    }
+
+    function fromDecimals(address tokenAddress, uint256 tokenCount)
+        public
+        view
+        returns (uint256)
+    {
+        uint8 decimals = ERC20Detailed(tokenAddress).decimals();
+        if(decimals < 18) {
+            return tokenCount.div(uint256(18 - decimals));
+        } else if (decimals > 18) {
+            return tokenCount.mul(uint256(decimals - 18));
+        } else {
+            return tokenCount;
+        }
+    }
+
+
 }
